b1bc4e234ff1b8d58b7b2c9fc895e649a263aceb
David Shupe <shupe@ipac.caltech.edu>
2017-02-05 16:14:33 -0800

    update version to beta3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='firefly_client',
-    version='1.0.0.b2',
+    version='1.0.0b3',
     description='Python API for Firefly',
     author='IPAC LSST SUIT',
     license='BSD',
@@ -16,6 +16,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ]
 )
 
