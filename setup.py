49d7032045b479e6b2bd65b23dca7e8adb67182c
David Shupe <shupe@ipac.caltech.edu>
2018-05-14 16:12:36 -0700

    bump major version for init changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='firefly_client',
-    version='1.3.0',
+    version='2.0.0-dev',
     description='Python API for Firefly',
     author='IPAC LSST SUIT',
     license='BSD',
