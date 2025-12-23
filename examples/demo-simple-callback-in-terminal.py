c2c1cb7b7317f0af8f98a2b7186dc759ad3da1df
Jaladh Singhal <jaladhsinghal@gmail.com>
2025-12-22 16:07:21 -0800

    Move callbacks demos from test/ to examples/
    Remove duplicate files

--- examples/demo-simple-callback-in-terminal.py
+++ examples/demo-simple-callback-in-terminal.py
@@ -1,21 +1,16 @@
 from firefly_client import FireflyClient
 import firefly_client
-import time
 
-lsst_demo_host = 'https://lsst-demo.ncsa.illinois.edu/firefly'
 local_host = 'http://127.0.0.1:8080/firefly'
 fd = 'https://fireflydev.ipac.caltech.edu/firefly'
 irsa_host = 'https://irsa.ipac.caltech.edu/irsaviewer'
 data_lsst_host = 'https://data.lsst.cloud/portal/app/'
-# host =  'https://fireflydev.ipac.caltech.edu/firefly-multi-ticket/firefly/'
 host =  local_host
-channel1 = 'channel-test-1'
 
 v_str = firefly_client.__dict__['__version__'] if '__version__' in firefly_client.__dict__ else 'development'
 print('Version: %s' % v_str)
-FireflyClient._debug = False
+# FireflyClient._debug = True  # enable for debug logging
 token = None
-# fc = FireflyClient.make_client(host, channel_override=channel1, launch_browser=True, token=token)
 fc = FireflyClient.make_client(host, launch_browser=True, token=token)
 print(fc.get_firefly_url())
 fc.show_fits(url="http://web.ipac.caltech.edu.s3-us-west-2.amazonaws.com/staff/roby/demo/wise-00.fits")
@@ -52,4 +47,4 @@ print('listener is added')
 # fc.remove_listener(example_listener)
 # time.sleep(2)
 # fc.add_listener(example_listener)
-fc.wait_for_events()
+fc.wait_for_events() # needed to keep the process alive to show callback output in terminal
